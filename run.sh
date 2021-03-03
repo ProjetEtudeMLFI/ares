6f8825a9834d0320cec3d741abd647e5683b3d3e
nguyen_marc <nguyen_marc@live.fr>
2021-03-03 15:32:12 +0000

    Fix

--- run.sh
+++ run.sh
@@ -1,36 +1,65 @@
-# Sample run.sh
-export PYTHONPATH=$(pwd)/:
+#!/bin/sh
+export ARESPATH=$(pwd)/ares
+export PYTHONPATH="${PYTHONPATH}:${ARESPATH}"
 
-TRAINED_MODELS_DIR="/ares/experiments/train/trained_models"
-PRETRAINED_WEIGHTS="/ares/quantized_weights"
+TRAINED_MODELS_DIR="${ARESPATH}/experiments/train/trained_models"
+PRETRAINED_WEIGHTS="${ARESPATH}/quantized_weights"
+CACHE_PATH="cache"
+RESULT_PATH="results"
+LOG_MNIST_PATH="logs/mnist_fc"
+CONF_PATH="conf"
+
 THEANO_FLAGS='device=gpu'
 SEED=0
 FRATE=0.05
 
-mkdir -p cache
-mkdir -p results
+# Build PATHS
+mkdir -p $CACHE_PATH
+mkdir -p $RESULT_PATH
 mkdir -p $PRETRAINED_WEIGHTS
 mkdir -p $TRAINED_MODELS_DIR
-mkdir -p logs/mnist_fc
+mkdir -p $LOG_MNIST_PATH
 
 # Training Model
-python /ares/experiments/train/train.py -c /ares/conf -m mnist_fc -eps 10 -v --results /ares/results -to -sw_name  $TRAINED_MODELS_DIR/mnist_fc
+python "${ARESPATH}/experiments/train/train.py" \
+    -c "${CONF_PATH}" \
+    -m mnist_fc \
+    -eps 10 \
+    -v \
+    --cache "${CACHE_PATH}" \
+    --results "${RESULT_PATH}" \
+    -to -sw_name "${TRAINED_MODELS_DIR}/mnist_fc"
 
 # Quantize Model
-python /ares/experiments/quantize/quantize_net.py -m mnist_fc -lw -ld_name $TRAINED_MODELS_DIR/mnist_fc -qi 2 -qf 6 --cache /ares/cache/ --conf /ares/conf --results /ares/results -sw /ares/quantized_weights/mnist_fc
+python ${ARESPATH}/experiments/quantize/quantize_net.py \
+    -m mnist_fc \
+    -lw \
+    -ld_name "${TRAINED_MODELS_DIR}/mnist_fc" \
+    -qi 2 \
+    -qf 6 \
+    --cache "${CACHE_PATH}" \
+    --conf "${CONF_PATH}" \
+    --results "${RESULT_PATH}" \
+    -sw "${PRETRAINED_WEIGHTS}/mnist_fc"
 
 # Testing pre-trained weights
-python /ares/experiments/eval/eval.py -m mnist_fc -v --results /ares/results -c /ares/conf -lw -ld_name $PRETRAINED_WEIGHTS/mnist_fc_quantized_2_6
+python "${ARESPATH}/experiments/eval/eval.py" \
+    -m mnist_fc  \
+    -v \
+    --results "${RESULT_PATH}" \
+    -c "${CONF_PATH}" \
+    -lw \
+    -ld_name "${PRETRAINED_WEIGHTS}/mnist_fc_quantized_2_6"
 
 # Fault-Injection
-fname="mnist_quantized_2_8_($FRATE)_$SEED"
-python /ares/experiments/bits/bits.py -c /ares/conf -m mnist_fc --results /ares/results -lw -qi 2 -qf 6 -ld_name $PRETRAINED_WEIGHTS/mnist_fc_quantized_2_6 -frate $FRATE -seed $SEED | tee -a logs/mnist_fc/$fname
-
-# Training MNIST
-#python3 /ares/run_models.py -m mnist_fc -eps 10 -v  -to -c /ares/conf -sw
-
-# Training CiFar
-#python /ares/run_models.py -m cifar10_vgg -eps 10 -v  -to -c /ares/conf
-
-#Training SVHN
-#python /ares/run_models.py -m imagenet_resnet50 -eps 10 -v  -to -c /ares/conf
+fname="mnist_fc_quantized_2_6_${FRATE}_${SEED}"
+python "${ARESPATH}/experiments/bits/bits.py" \
+    -c "${CONF_PATH}" \
+    -m mnist_fc \
+    --results "${RESULT_PATH}" \
+    -lw \
+    -qi 2 \
+    -qf 6 \
+    -ld_name "${PRETRAINED_WEIGHTS}/mnist_fc_quantized_2_6" \
+    -frate "${FRATE}" \
+    -seed "${SEED}" | tee -a "${LOG_MNIST_PATH}/$fnamemkdir" -p results
